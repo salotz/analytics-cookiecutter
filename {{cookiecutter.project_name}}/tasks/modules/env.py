24fcf073a1ffab5641bbba6ff21aa94b47b91d43
Samuel D Lotz <samuel.lotz@salotz.info>
2019-11-19 15:39:19 -0500

    update

--- {{cookiecutter.project_name}}/tasks/modules/env.py
+++ {{cookiecutter.project_name}}/tasks/modules/env.py
@@ -13,11 +13,15 @@ def build(cx, name='common'):
     cx.run(f"conda create -y -n {env_name} python={PY_VERSION}",
         pty=True)
 
+
     # install the pip dev dependencies
     cx.run(f"$ANACONDA_DIR/envs/{env_name}/bin/pip install -r configs/{name}.requirements.txt")
 
     # install the conda dev dependencies
     cx.run(f"conda env update -n {env_name} --file configs/{name}.env.yaml")
+
+    # install the pip tools dependencies
+    cx.run(f"$ANACONDA_DIR/envs/{env_name}/bin/pip install -r tools.requirements.txt")
 
     print("--------------------------------------------------------------------------------")
     print(f"run: conda activate {env_name}")
